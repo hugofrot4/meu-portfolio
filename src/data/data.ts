fdb60147fff6968a418e882c0329ca71312d5f01
Hugo Frota <herbtyh@gmail.com>
2026-05-06 23:30:18 -0300

    feat: adicionar array de projetos

---
+++ src/data/data.ts
@@ -0,0 +1,79 @@
+export interface Project {
+  image: string;
+  title: string;
+  description: string;
+  productionUrl?: string;
+  repositoryUrl?: string;
+  category: "Wordpress" | "React / Estudos";
+}
+
+export const projects: Project[] = [
+  {
+    image: "../assets/humanize.png",
+    title: "Instituto Humanize",
+    description:
+      "Projeto de website de instituição feito em Wordpress usando HTML, SASS, Javascript, Jquery e Swiper",
+    productionUrl: "https://www.ihumanize.org/",
+    category: "Wordpress",
+  },
+  {
+    image: "../assets/iguatemi.png",
+    title: "Iguatemi",
+    description:
+      "Projeto de website para shopping center feito em Wordpress usando HTML, SASS, Javascript, Jquery e Swiper",
+    productionUrl: "https://www.iguatemibosque.com.br/",
+    category: "Wordpress",
+  },
+  {
+    image: "../assets/duopass.png",
+    title: "Duopass",
+    description:
+      "Projeto de website com foco no turismo feito em Wordpress usando HTML, SASS, Javascript, Jquery e Swiper",
+    productionUrl: "https://duopass.com.br/",
+    category: "Wordpress",
+  },
+  {
+    image: "../assets/bsurbanismo.png",
+    title: "BS Urbanismo",
+    description:
+      "Projeto de website para empresa de arquitetura e urbanismo feito em Wordpress usando HTML, SASS, Javascript, Jquery e Swiper",
+    productionUrl: "https://www.bsparurbanismo.com.br/",
+    category: "Wordpress",
+  },
+  {
+    image: "../assets/dogs.jpg",
+    title: "Dogs",
+    description:
+      "Projeto de rede social fictícia para cachorros realizado durante o curso de React da Origamid, usando React Hooks, Consumo de API com o Fetch e CSS modules",
+    productionUrl: "https://hugocodes-dogs.netlify.app/",
+    repositoryUrl: "https://github.com/hugofrot4/dogs",
+    category: "React / Estudos",
+  },
+  {
+    image: "../assets/mytasks.png",
+    title: "My Tasks",
+    description:
+      "Projeto de um gerenciador de tarefas com funções de edição, remoção e clonclusão das mesmas feito em React (Layout desenhado pro mim no Figma), usando React Hooks, Local Storage e SASS",
+    productionUrl: "https://hugocodes-mytasks.netlify.app/",
+    repositoryUrl: "https://github.com/hugofrot4/my-tasks",
+    category: "React / Estudos",
+  },
+  {
+    image: "../assets/chamados.png",
+    title: "Chamados",
+    description:
+      "Projeto de um dashboard de chamados com login, paginação, adição de clientes e tickets feito em React, React Hooks, Firebase, React Router Dom e SASS",
+    productionUrl: "https://hugocodes-chamados.netlify.app/",
+    repositoryUrl: "https://github.com/HugoCodesBr/chamados",
+    category: "React / Estudos",
+  },
+  {
+    image: "../assets/movieslib.png",
+    title: "MoviesLib",
+    description:
+      "Projeto de uma catálogo de filmes com funções de busca e página internas feito em React consumindo a API do TMDB, usando React Hooks, React Router Dom e Fetch",
+    productionUrl: "https://hugocodes-movieslib.netlify.app/",
+    repositoryUrl: "https://github.com/HugoCodesBr/movies_lib",
+    category: "React / Estudos",
+  },
+];
