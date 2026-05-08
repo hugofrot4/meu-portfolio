abd0100673738931e21eeca692e59d90ca0274e6
Hugo Frota <herbtyh@gmail.com>
2026-05-08 11:27:26 -0300

    feat: adicionar seção hero

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,14 @@ import { Lightbulb, LightbulbOff, Menu, X } from "lucide-react";
 const menuItems = ["Sobre mim", "Tecnologias", "Projetos", "Contato"];
 
 export default function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
   const { theme, toggleTheme } = useContext(ThemeContext);
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-    <header className="bg-background text-dark p-4 fixed top-0 w-full">
+    <header className="bg-background text-dark p-4 fixed top-0 w-full z-50">
       <div className="xl:container xl:mx-auto xl:flex xl:justify-between xl:items-center">
         <div className="flex items-center justify-between">
           <div>
@@ -30,11 +28,11 @@ export default function Header() {
           </div>
         </div>
         <div
-          className={`${menuOpen ? "" : "translate-x-full"} transition duration-200 w-full absolute top-19 left-0 border-t-2 border-dark xl:translate-0 xl:static xl:border-none xl:w-max xl:flex xl:items-center`}
+          className={`${menuOpen ? "" : "translate-x-full"} bg-background transition duration-200 w-full absolute top-19 left-0 border-t-2 border-dark xl:translate-0 xl:static xl:border-none xl:w-max xl:flex xl:items-center xl:bg-transparent`}
         >
           <ul className="xl:flex">
             {menuItems.map((item) => (
-              <li>
+              <li key={item}>
                 <a
                   className="w-full flex justify-center p-2 border-b border-gray-100 xl:border-none xl:p-0 xl:ml-4 xl:w-max xl:relative xl:after:content-[''] xl:after:h-0.5 xl:after:bg-dark xl:after:absolute xl:after:bottom-0 xl:after:right-0 xl:after:w-0 hover:xl:after:w-full xl:after:transition-all xl:after:duration-300 xl:after:ease-in-out"
                   href="#"
---
+++ src/components/Hero.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import imagemHugo from "../assets/hugo.png";
+import { MoveDown } from "lucide-react";
+
+interface AnimatedNumberProps {
+  value: number;
+  duration?: number;
+}
+
+const AnimatedNumber = ({ value, duration = 2000 }: AnimatedNumberProps) => {
+  const [displayNumber, setDisplayNumber] = useState(0);
+
+  useEffect(() => {
+    let startTime: number | null = null;
+    let frameId: number;
+
+    const step = (timestamp: number) => {
+      if (!startTime) startTime = timestamp;
+      const progress = timestamp - startTime;
+      const percentage = Math.min(progress / duration, 1);
+
+      const easeOutQuad = 1 - (1 - percentage) * (1 - percentage);
+
+      setDisplayNumber(Math.floor(easeOutQuad * value));
+
+      if (progress < duration) {
+        frameId = requestAnimationFrame(step);
+      }
+    };
+
+    frameId = requestAnimationFrame(step);
+    return () => cancelAnimationFrame(frameId);
+  }, [value, duration]);
+
+  return <>{displayNumber}</>;
+};
+
+interface Xp {
+  number: number;
+  description: string;
+}
+
+const xp: Xp[] = [
+  {
+    number: 50,
+    description: "projetos entregues",
+  },
+  {
+    number: 6,
+    description: "anos como dev frontend",
+  },
+];
+
+export default function Hero() {
+  return (
+    <div className="min-h-screen pt-19 flex flex-col relative xl:container xl:mx-auto">
+      <div className="px-2 text-center flex flex-col gap-2 mt-10 text-dark xl:text-left xl:px-40 xl:mt-40">
+        <p className="text-3xl xl:text-6xl">Olá, eu sou o Hugo!</p>
+        <p className="font-light tracking-widest xl:text-3xl">
+          Desenvolvedor Frontend
+        </p>
+      </div>
+      <div className="text-dark px-2 flex justify-center gap-4 pt-10 xl:justify-start xl:px-40 xl:pt-15">
+        {xp.map((item) => (
+          <div key={item.description} className="flex flex-col">
+            <p className="text-5xl xl:text-7xl">
+              +<AnimatedNumber value={item.number} duration={2000} />
+            </p>
+            <p className="max-w-25 ml-7 xl:ml-11">{item.description}</p>
+          </div>
+        ))}
+      </div>
+      <div className="hidden xl:flex items-center px-40 pt-10 gap-3 animate-pulse text-dark">
+        <p className="text-xl">Role a página </p>
+        <MoveDown />
+      </div>
+      <div className="absolute bottom-0 left-0 right-0 xl:left-[unset] xl:w-[60%]">
+        <img src={imagemHugo} alt="Hugo" />
+      </div>
+    </div>
+  );
+}
